c94efb3d57f7be52fb21fcaf6ff8ce0ef792bbe0
Victor C.B. <victor.charneco.barba@gmail.com>
2025-11-19 13:12:20 +0100

    files to be refactored uploaded

---
+++ classes/Character.php
@@ -0,0 +1,16 @@
+<?php
+
+class Character
+{
+    public function move(): void
+    {
+        echo "The character moves forward.\n";
+    }
+
+    public function attack(): void
+    {
+        echo "The character attacks with a sword.\n";
+    }
+}
+
+?>
